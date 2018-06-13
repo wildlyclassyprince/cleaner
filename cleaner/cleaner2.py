d03869b5c2104648a474faa7492e4a24d40c3fb9
wildlyclassyprince <wildlyclassyprince@users.noreply.github.com>
2018-06-13 16:13:48 +0200

    Added frequency_plot

--- cleaner/cleaner2.py
+++ cleaner/cleaner2.py
@@ -3,16 +3,20 @@
 # The suspects ...
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import seaborn as sns
 
 # Information
 __author__ = "wildlyclassyprince"
 __version__ = "$Revision: 0.1.1 $"
+__date__ = "$Date: Wed Jun 13 15:50:36 CAT 2018 $"
 __licence__ = "GNU"
 
-# Unique values
-def unique_values(dataframe):
+# Value counts
+def unique_value_counts(dataframe):
 	'''
-	Prints the unique values for each column.
+	Prints the unique value counts for each column.
 	'''
 	_cols = (x for x in dataframe.columns)
 	try:
@@ -26,23 +30,6 @@ def unique_values(dataframe):
 				  '\n')
 	except StopIteration:
 		print('Done.')
-	except KeyError:
-		print('Nothing to do.\n')
-		
-# Unique value counts
-def unique_value_counts(dataframe):
-	'''
-	Prints the unique value counts for each column.
-	'''
-	_cols = (x for x in dataframe.columns)
-	try:
-		while True:
-			field = _cols.__next__()
-			print('='*50, 
-				  '\nColumn: "{}"\nCount:\t{}\n'.format(field, len(dataframe[field].unique())),
-				  '='*50, '\n')
-	except StopIteration:
-		print('Done.\n')
 	except KeyError:
 		print('Nothing to do.\n')
 
@@ -62,6 +49,8 @@ def word_case(dataframe, font='Title'):
 					dataframe[column] = dataframe[column].str.lower()
 				elif (font.lower() == 'upper'):
 					dataframe[column] = dataframe[column].str.upper()
+				elif (font.lower() == 'sentence'):
+					dataframe[column] = dataframe[column].str.capitalize()
 				else:
 					pass
 	except StopIteration:
@@ -81,7 +70,7 @@ def drop_fields(fields, dataframe):
 		while True:
 			del dataframe[_drop.__next__()]
 	except StopIteration:
-		print('\nDone!\nNew dataframe shape: {}\n\nThe new dataframe:\n'.format(dataframe.shape))
+		print('\nDone!\nNew dataframe shape: {}\n\nThis is what the dataframe looks like now:\n'.format(dataframe.shape))
 	except KeyError:
 		print('Column does not exist.')
 		pass
@@ -120,3 +109,42 @@ def remove_columns(dataframe):
 		print('Final shape of the dataset:{}'.format(dataframe.shape))
 
 	return dataframe.head()
+
+# Frequency plot
+def frequency_plot(dataframe, x, title, xlabel, hue=None):
+    # Plot settings
+    plt.rcParams['figure.figsize'] = (16.0, 9.0)
+    # Size of dataframe
+    ncount=len(dataframe)
+    # Our default chart
+    ax=sns.countplot(data=dataframe, x=x, hue=hue)
+    plt.title(title, fontsize=16)
+    plt.xlabel(xlabel)
+    # Making the twin axes
+    ax2=ax.twinx()
+    # Switching axes so that percentage is on the left and frequency on the right
+    ax2.yaxis.tick_left()
+    ax.yaxis.tick_right()
+    # We also need to switch the labels
+    ax.yaxis.set_label_position('right')
+    ax2.yaxis.set_label_position('left')
+    # Labelling our axes
+    ax2.set_ylabel('Percentage')
+    ax.set_ylabel('Frequency Count')
+    for p in ax.patches:
+        x=p.get_bbox().get_points()[:,0]
+        y=p.get_bbox().get_points()[1,1]
+        # Setting text alignment
+        ax.annotate('{:.1f}%'.format(100.*y/ncount), (x.mean(), y), ha='center', va='bottom')
+    # Use a LinearLocator to ensure the correct number of ticks
+    ax.yaxis.set_major_locator(ticker.LinearLocator(11))
+    # Setting the y-axis ticks
+    ax2.yaxis.set_ticks(np.arange(0, 101, 10))
+    ax2.yaxis.set_major_formatter(ticker.FormatStrFormatter('%.0f%%'))
+    # Setting the axes range
+    ax2.set_ylim(0, 100)
+    ax.set_ylim(0, ncount)
+    # Use a MultipleLocator to ensure a tick spacing of 10
+    ax2.yaxis.set_major_locator(ticker.MultipleLocator(10))
+    # Grid lines
+    ax2.grid(None)
