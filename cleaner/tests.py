0d75d623e61a9bafac6c57d89d9ce83a7d6fb874
Mthuli Bhebhe <wildlyclassyprince@users.noreply.github.com>
2018-09-30 15:33:00 +0200

    Updated `tests.py`
    Scripts merely tests the size of the dataframe. Need to create a test dataframe for evaluating success of the tests.

--- cleaner/tests.py
+++ cleaner/tests.py
@@ -14,3 +14,18 @@ __status__ = "Initial Tests"
 # The suspects
 import pandas as pd
 import numpy as np
+
+# Test size of dataframe
+def test_size_of_dataframe_rows():
+    '''
+    Tests the size of the dataframe.
+    Dataframe should not be empty.
+    '''
+    assert df.shape[1] > 0
+    
+def test_size_of_dataframe_columns():
+    '''
+    Tests the size of the dataframe.
+    Dataframe should have more that one column.
+    '''
+    assert df.shape[0] > 1
