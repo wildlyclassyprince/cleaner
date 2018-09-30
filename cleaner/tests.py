e17e6a66efa25bfac2f3254e0bd513d2da034c63
Mthuli Bhebhe <wildlyclassyprince@users.noreply.github.com>
2018-09-30 15:26:43 +0200

    Tests for `cleaner.py`

---
+++ cleaner/tests.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+'''
+This script contains tests for `cleaner.py`.
+'''
+
+__author__ = "wildlyclassyprince"
+__license__ = "GNU"
+__version__ = "0.1.0"
+__maintainer__ = "wildlyclassyprince"
+__email__ = "lihtumb@gmail.com"
+__status__ = "Initial Tests"
+
+# The suspects
+import pandas as pd
+import numpy as np
