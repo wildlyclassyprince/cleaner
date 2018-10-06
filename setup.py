641578f2e6452a9d301691e1f5bffcfcbe52f706
Mthuli Bhebhe <wildlyclassyprince@users.noreply.github.com>
2018-10-06 12:31:24 +0200

    New edits

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+		name = 'cleaner',
+		packages = ['cleaner'],
+		install_requires = ['numpy', 'pandas'],
+		py_modules = ['cleaner'],
+		version = '0.1.1',
+		description = 'A module for simplified data cleaning.',
+		author = 'wildlyclassyprince',
+		author_email = 'lihtumb@gmail.com',
+		url = 'https://github.com/wildlyclassyprince/cleaner',
+		classifiers = []
+)
